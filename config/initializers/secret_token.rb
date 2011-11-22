4908b341bd2d37f3c787aa0154ccbbf67c558390
Anh Tran <atran.wynd@gmail.com>
2011-11-22 20:48:39 +0800

    My first backup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knownagain::Application.config.secret_token = 'f708e52dfe8452d75e4234a3f168b0a24c846bbeba51e25c67c86416f240852ae2ef62c8519267790ef351ed2712d16af9b35260d4d5d6d4a831364f4bb75fd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Knownagain::Application.config.session_store :cookie_store, key: '_knownagain_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Knownagain::Application.config.session_store :active_record_store
